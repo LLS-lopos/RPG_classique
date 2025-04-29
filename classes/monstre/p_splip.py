571bc20414eefcbaadc2b08157e93e83f009d94e
LLS-lopos <luderickls@gmail.com>
2025-04-29 15:38:05 -0400

    ajout de classe de monstre

---
+++ classes/monstre/p_splip.py
@@ -0,0 +1,17 @@
+from random import randint
+
+from classes.perso import personnage
+
+class Splip(personnage):
+    def __init__(self, nom: str="splip", pv: int=6, att: int=2, ko: bool=False, val_exp: int=10):
+        personnage.__init__(self, nom, pv, att, ko)
+        self.nom = nom
+        self.pv = pv
+        self.att = att
+        self.ko = ko
+        self.valeur_exp = val_exp
+
+        self.exp = 0
+        self.exp_max = 100
+        self.niv = 0
+        self.niv_max = 20
